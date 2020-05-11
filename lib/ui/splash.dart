acdb4137c6233a06e1343910b32531cabe4f9946
Gagan <gaganyadav80@gmail.com>
2020-05-11 12:23:55 +0530

    Updated the splash UI

--- lib/ui/splash.dart
+++ lib/ui/splash.dart
@@ -1,32 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-          color: Colors.white,
-          child: Container(
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                CircularProgressIndicator(
-                  // backgroundColor: Colors.black,
-                  valueColor: new AlwaysStoppedAnimation<Color>(Colors.black),
+    return Scaffold(
+      body: Container(
+        color: Colors.black,
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            children: <Widget>[
+              const SizedBox(height: 350.0),
+              // CircularProgressIndicator(
+              //   backgroundColor: Colors.black,
+              //   valueColor: new AlwaysStoppedAnimation<Color>(Colors.white),
+              // ),
+              SpinKitChasingDots(
+                color: Colors.white,
+              ),
+              const SizedBox(height: 280),
+              Text(
+                "Checking your Google Login Status",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20.0,
                 ),
-                const SizedBox(height: 10.0),
-                Text(
-                  "Loading ...",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontFamily: 'SourceSansPro - Light',
-                    fontSize: 18.0,
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
