3ac94401e0e33e397ca502420c54e31289cff381
Gagan <gaganyadav80@gmail.com>
2020-05-11 12:25:35 +0530

    Added flutter_spinkit

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,24 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:givnotes/utils/noteInherited.dart';
 
 import 'pages/home.dart';
 import 'ui/splash.dart';
 import 'utils/login.dart';
+
+bool isSkipped = false;
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return NoteInherited(
-          child: MaterialApp(
-        theme: ThemeData(
-          primaryColor: Colors.black,
-        ),
-        themeMode: ThemeMode.system,
-        home: CheckLogIn(),
-      ),
+    return MaterialApp(
+      // theme: ThemeData(
+      //   primaryColor: Colors.black,
+      // ),
+      themeMode: ThemeMode.system,
+      home: CheckLogIn(),
     );
   }
 }
@@ -30,6 +29,7 @@ class CheckLogIn extends StatelessWidget {
     return StreamBuilder(
       stream: FirebaseAuth.instance.onAuthStateChanged,
       builder: (context, AsyncSnapshot<FirebaseUser> snapshot) {
+        if(isSkipped == true) return HomePage();
         if (snapshot.connectionState == ConnectionState.waiting) return SplashPage();
         if (!snapshot.hasData || snapshot.data == null) return LoginPage();
 
