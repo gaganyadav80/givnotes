2d17281c8509539e871e28ba068a6188f4059526
Gagan <gaganyadav80@gmail.com>
2020-05-12 20:02:19 +0530

    updated splash screen

--- lib/ui/splash.dart
+++ lib/ui/splash.dart
@@ -6,24 +6,44 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.black,
+        color: Colors.white,
         child: Container(
           width: double.infinity,
           child: Column(
             children: <Widget>[
-              const SizedBox(height: 350.0),
-              // CircularProgressIndicator(
-              //   backgroundColor: Colors.black,
-              //   valueColor: new AlwaysStoppedAnimation<Color>(Colors.white),
-              // ),
+              Padding(
+                padding: const EdgeInsets.only(top: 80, left: 20),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      'Giv',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'Montserrat',
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Notes.',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'Montserrat',
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 250.0),
               SpinKitChasingDots(
-                color: Colors.white,
+                color: Colors.deepOrangeAccent[400],
               ),
               const SizedBox(height: 280),
               Text(
                 "Checking your Google Login Status",
                 style: TextStyle(
-                  color: Colors.white,
+                  color: Colors.black,
                   fontFamily: 'SourceSansPro',
                   fontSize: 20.0,
                 ),
