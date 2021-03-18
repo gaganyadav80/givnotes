1c53f4b8ad5db8cd8b254b5115c4a2a2e3e538ef
Gagan <gaganyadav80@gmail.com>
2021-03-18 21:24:27 +0530

    Testing sublime merge password caching

--- lib/main.dart
+++ lib/main.dart
@@ -104,8 +104,6 @@ class GivnotesApp extends StatelessWidget {
         'settings_p': (context) => SettingsPage(),
         'search_p': (context) => SearchPage(),
       },
-      // locale: DevicePreview.locale(context),
-      // builder: DevicePreview.appBuilder,
       title: 'Givnotes',
       theme: ThemeData(
         //maybe switch to google fonts
@@ -114,20 +112,6 @@ class GivnotesApp extends StatelessWidget {
         accentColorBrightness: Brightness.light,
         toggleableActiveColor: Colors.blue,
       ),
-      // builder: (context, widget) => ResponsiveWrapper.builder(
-      //   BouncingScrollWrapper.builder(context, widget),
-      //   maxWidth: 1200,
-      //   minWidth: 300,
-      //   defaultScale: false,
-      //   breakpoints: [
-      //     ResponsiveBreakpoint.resize(300, name: MOBILE),
-      //     ResponsiveBreakpoint.autoScale(800, name: TABLET),
-      //     ResponsiveBreakpoint.autoScale(1000, name: TABLET),
-      //     ResponsiveBreakpoint.resize(1200, name: DESKTOP),
-      //     ResponsiveBreakpoint.autoScale(2460, name: "4K"),
-      //   ],
-      //   background: Container(color: Color(0xFFF5F5F5)),
-      // ),
       // builder: (context, widget) => BlocListener<AuthenticationBloc, AuthenticationState>(
       //   child: widget,
       //   listener: (context, state) {
